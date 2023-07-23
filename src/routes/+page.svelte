4c6b0a5cee155457ba7ae3ab8e0a50a5a6c5346a
Christian Xyox <warsperu@gmail.com>
2023-07-23 16:05:10 -0500

    main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,37 @@
 <script>
-	import { onDestroy, onMount } from "svelte";
-	import {
-		Bookshop,
-		trackBookshopLiveData
-	 } from "@bookshop/sveltekit-bookshop";
-	import {
-		onCloudCannonChanges,
-		stopCloudCannonChanges,
-	} from "@cloudcannon/svelte-connector";
-	import { site_title } from '@data/site.json';
+    import { onDestroy, onMount } from 'svelte';
+    import {
+        Bookshop,
+        trackBookshopLiveData,
+    } from '@bookshop/sveltekit-bookshop';
+    import {
+        onCloudCannonChanges,
+        stopCloudCannonChanges,
+    } from '@cloudcannon/svelte-connector';
+    import { site_title } from '@data/site.json';
 
-	export let data;
+    export let data;
 
-	let pageDetails = data.data;
+    let pageDetails = data.data;
 
-	let title = `${site_title}${pageDetails.title ? ` | ${pageDetails.title}` : ''}`;
+    let title = `${site_title}${
+        pageDetails.title ? ` | ${pageDetails.title}` : ''
+    }`;
 
-	onMount(async () => {
-		onCloudCannonChanges(
-			(newProps) => (pageDetails = trackBookshopLiveData(newProps))
-		);
-	});
-	onDestroy(async () => {
-		stopCloudCannonChanges();
-	});
-
+    onMount(async () => {
+        onCloudCannonChanges(
+            (newProps) => (pageDetails = trackBookshopLiveData(newProps))
+        );
+    });
+    onDestroy(async () => {
+        stopCloudCannonChanges();
+    });
 </script>
 
 <svelte:head>
-	<title>{title}</title>
+    <title>hola {title}</title>
 </svelte:head>
 
 <div>
-	<Bookshop shared="page" content_blocks={pageDetails.content_blocks} />
+    <Bookshop shared="page" content_blocks={pageDetails.content_blocks} />
 </div>
